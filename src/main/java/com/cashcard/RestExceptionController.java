302b466ca6ec40f7bd5bef0894b76648dbc8ba48
mrizkisaputra <mrizkisaputra6@gmail.com>
2025-05-21 17:34:01 +0700

    implement request id filter

--- src/main/java/com/cashcard/RestExceptionController.java
+++ src/main/java/com/cashcard/RestExceptionController.java
@@ -2,7 +2,9 @@ package com.cashcard;
 
 import com.cashcard.dto.apiError.ApiErrorResponse;
 import com.cashcard.dto.apiError.ApiValidationErrorResponse;
+import com.cashcard.filters.RequestIdFilter;
 import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
@@ -21,6 +23,7 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 @RestControllerAdvice
+@Slf4j
 public class RestExceptionController extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler({ResponseStatusException.class})
@@ -30,7 +33,18 @@ public class RestExceptionController extends ResponseEntityExceptionHandler {
                 .message(ex.getMessage())
                 .timestamp(LocalDateTime.now())
                 .instance(request.getRequestURI())
+                .requestId(request.getAttribute(RequestIdFilter.REQUEST_ID).toString())
                 .build();
+
+        // ✅ Logging error response
+        log.warn("RequestId: {}, IPAddress:{}, Path: {}, Method: {}, Causes={}",
+                request.getAttribute(RequestIdFilter.REQUEST_ID),
+                request.getRemoteAddr(),
+                request.getRequestURI(),
+                request.getMethod(),
+                ex.getLocalizedMessage()
+        );
+
         return ResponseEntity.status(apiError.getStatus()).body(apiError);
     }
 
@@ -42,16 +56,28 @@ public class RestExceptionController extends ResponseEntityExceptionHandler {
             WebRequest request
     ) {
         ServletWebRequest servletWebRequest = (ServletWebRequest) request;
+        HttpServletRequest servletRequest = servletWebRequest.getRequest();
         List<FieldError> fieldErrors = ex.getBindingResult().getFieldErrors();
         List<ObjectError> globalErrors = ex.getBindingResult().getGlobalErrors();
         ApiErrorResponse apiError = ApiErrorResponse.builder()
                 .status(HttpStatus.BAD_REQUEST)
                 .message("Validation Failed")
                 .timestamp(LocalDateTime.now())
-                .instance(servletWebRequest.getRequest().getRequestURI())
+                .instance(servletRequest.getRequestURI())
+                .requestId(servletRequest.getAttribute(RequestIdFilter.REQUEST_ID).toString())
                 .build();
         apiError.addFieldErrors(fieldErrors);
         apiError.addGlobalErrors(globalErrors);
+
+        // ✅ Logging error response
+        log.warn("RequestId: {}, IPAddress:{}, Path: {}, Method: {}, Causes={}",
+                servletRequest.getAttribute(RequestIdFilter.REQUEST_ID),
+                servletRequest.getRemoteAddr(),
+                servletRequest.getRequestURI(),
+                servletRequest.getMethod(),
+                ex.getLocalizedMessage()
+        );
+
         return ResponseEntity.status(apiError.getStatus()).body(apiError);
     }
 }
---
+++ src/main/java/com/cashcard/filters/RequestIdFilter.java
@@ -0,0 +1,33 @@
+package com.cashcard.filters;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.util.UUID;
+
+@Component
+public class RequestIdFilter extends OncePerRequestFilter {
+    public static final String REQUEST_ID = "requestId";
+
+    @Override
+    protected void doFilterInternal(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            FilterChain filterChain
+    ) throws ServletException, IOException {
+
+        String requestId = request.getHeader("X-Request-Id");
+        if (requestId == null || requestId.isBlank()) {
+            requestId = UUID.randomUUID().toString();
+        }
+        // simpan di attribute request
+        request.setAttribute(REQUEST_ID, requestId);
+        filterChain.doFilter(request, response);
+    }
+
+}
